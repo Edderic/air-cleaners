90fad915870303ccee2f71d80847cbcf84756315
Edderic Ugaddan <edderic@gmail.com>
2024-10-15 12:32:28 -0400

    create html file that has new serial numbers

--- python/gen_uuid.py
+++ python/gen_uuid.py
@@ -1,5 +1,30 @@
 from uuid import uuid4
 
+def printable(version = "V2.0.0", serial=""):
+    return f"""
+        <div>
+          <p>LaminAir {version}</p>
+          <p>Serial: {str(serial)[0:8]}</p>
+          <p>https://breathesafe-llc.myshopify.com</p>
+
+          <br>
+          <br>
+          <br>
+
+        </div>
+    """
 if __name__ == "__main__":
-    for i in range(5):
-        print(uuid4())
+    file = open("serials.html", 'w')
+
+    lines = [
+        "<html>",
+        "<body>"
+    ]
+
+    for i in range(10):
+        lines.append(
+            printable(serial=uuid4())
+        )
+
+    file.writelines(lines)
+    file.close()
